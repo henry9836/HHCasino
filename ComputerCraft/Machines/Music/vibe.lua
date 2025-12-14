8b80670941a3c6307476002dad4e14479c66f9f3
Henry <henry983615@gmail.com>
2025-12-14 20:09:35 +1300

    vibing 2.0

--- ComputerCraft/Machines/Music/vibe.lua
+++ ComputerCraft/Machines/Music/vibe.lua
@@ -4,10 +4,22 @@ local speaker = peripheral.find("speaker")
 local decoder = dfpwm.make_decoder()
 local h = fs.open("music.dfpwm", "rb")
 
+local upsampleFactor = 6 -- 8k -> 48k
+
 while true do
-    local chunk = h.read(16 * 1024)
+    local chunk = h.read(1024)  -- small chunks
     if not chunk then break end
-    speaker.playAudio(decoder(chunk))
+    local decoded = decoder(chunk)  -- decode 8k chunk
+
+    -- upsample by repeating each sample
+    local upsampled = {}
+    for i = 1, #decoded do
+        for j = 1, upsampleFactor do
+            upsampled[#upsampled+1] = decoded:sub(i,i)
+        end
+    end
+
+    speaker.playAudio(table.concat(upsampled))
     os.pullEvent("speaker_audio_empty")
 end
 
